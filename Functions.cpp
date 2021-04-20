3a73fd28f3fe6bad9439e2155971c13d16bdba76
KristianCrosby <kristian.crosby.18@cnu.edu>
2021-04-19 23:22:37 -0400

    finished functions

--- Functions.cpp
+++ Functions.cpp
@@ -27,16 +27,25 @@ std::vector<std::unique_ptr<Smalltalk>> getPeople(int numBrit,
 	//create a vector to hold SmallTalk unique pointers
 		vector<unique_ptr<Smalltalk>> smallTalkVector;
 		//add brits to vector
-		for (int i = 0;)
+		for (int i = 0; i < numBrit; i++){
+			Smalltalk_Brit brit;
+			smallTalkVector(brit);
+		}
 		//add americans  to vector
-
+		for (int i = 0; i < numAmerican; i++){
+					Smalltalk_Brit american;
+					smallTalkVector(american);
+				}
 		//add american donut enthusiest  to vector
-
+		for (int i = 0; i < numbAmericanDonutEnthusiest; i++){
+					Smalltalk_Brit nADE;
+					smallTalkVector(nADE);
+				}
 		//create some watches (as long as number watches <= numb people)
 		//then give the watches away to first NUM_WATCHES people in the vector
 		// when you are finished using the vector you return
 		//from this function(see Smalltalk header for hints)
-		while numWatches < numb
+		int people = numAmerican + numbAmericanDonutEnthusiest + numBrit;
 		//return your vector
 		return smallTalkVector;
 }
